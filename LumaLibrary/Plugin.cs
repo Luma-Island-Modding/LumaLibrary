cf1765e34a5370125fee0e01d63d16d1247cf8d3
Egor <ItsAwware@yandex.ru>
2024-11-25 07:30:45 +0600

    build: init

---
+++ LumaLibrary/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace LumaLibrary;
+
+[BepInPlugin(ModGUID, ModName, Version)]
+public class Plugin : BaseUnityPlugin
+{
+    public const string ModGUID = "etc.shikaru.lumalib";
+    public const string ModName = "LumaLibrary";
+    public const string Version = "0.0.1";
+
+    internal static new ManualLogSource Logger;
+        
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
